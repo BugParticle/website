eee7faf0ceee1b89bf8943b7ef266e7d0b669921
Chris DeForest <ceddeforest@gmail.com>
2024-05-18 13:09:12 -0400

    uploading files from local storage
    website has a functioning flexible navigation bar

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/* clickable <li> elements */
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelectorAll('.clickable-li').forEach(item=>{
+        item.addEventListener('click', function(){
+            window.location.href = this.getAttribute('data-url');
+        });
+    });
+});
